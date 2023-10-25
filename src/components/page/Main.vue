c2955a63bb0daf4633f9178ff0a136735bc8c9c1
sahong <sahong@lkbt.co.kr>
2023-10-25 17:37:07 +0900

    [추가] css추가

--- src/components/page/Main.vue
+++ src/components/page/Main.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="container">
         <h2>메뉴고르기</h2>
-        <h3></h3>
+        <h3>🍕🍔🍟🌭🍿🥞🥖🫓🥪🍗🥟</h3>
         <div class="version">
             <div class="form">
                 <input v-model="newMenu" class="input" placeholder="메뉴를 입력하세요" @keyup.enter="fnRegister"/>
@@ -35,6 +35,71 @@ import { MainJs } from '@/resources/js';
 export default MainJs;
 </script>
 
-<style scoped>
+<style scoped >
+.container {
+    min-height: 600px;
+    margin-top: 100px;
+    /* display: flex;
+    align-items: center;
+    justify-content: center; 
+    flex-direction: column; */
 
+    .form {
+        padding: 20px 0 20px 0;
+        height: 100px;
+
+        .input {
+            margin-right: 10px; 
+            height: 50px;
+            padding: 0 5px; 
+            font-size: 15px;
+        }
+
+        .registerBtn {
+            margin-right: 10px;
+            height: 50px;
+            width: 40px;
+            font-size: 15px;
+        }
+        .setValueBtn { 
+            height: 50px;
+            width: 50px; 
+            font-size: 15px;
+    }
+
+}
+
+.confirm {
+    margin: 10px 0;
+
+    .confirmBtn {
+        width: 70px;
+        height: 30px;
+    }
+}
+.reset {
+    margin: 10px 0;
+    .resetBtn {
+        width: 70px;
+        height: 30px;
+    }
+}
+
+.menu {
+    min-height: 200px;
+    display: flex;
+    flex-direction: column;
+    .menuitem {
+        display: flex;
+        margin: 5px 0;
+        min-width: 100px;
+    }
+    
+
+}
+
+.result {
+    margin-top: 10px;
+}
+}
 </style>
