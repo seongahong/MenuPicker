d7709818a1b2ffe443f6d339b126459bd34bb98e
sahong <sahong@lkbt.co.kr>
2023-10-25 23:24:42 +0900

    [수정] 스타일 수정

--- src/components/page/Main.vue
+++ src/components/page/Main.vue
@@ -1,105 +1,191 @@
 <template>
-    <div class="container">
-        <h2>메뉴고르기</h2>
-        <h3>🍕🍔🍟🌭🍿🥞🥖🫓🥪🍗🥟</h3>
-        <div class="version">
-            <div class="form">
-                <input v-model="newMenu" class="input" placeholder="메뉴를 입력하세요" @keyup.enter="fnRegister"/>
-                <button class="registerBtn" @click="fnRegister">등록</button>
-                <button class="setValueBtn" @click="fnSet">기본값</button>
-            </div>
-            <div class="menu">
-                <div  class="menuItem" v-for="(item, idx) in menuGroup" :key="idx" >
-                    <ul class="text">
-                        <li>{{ item.name }}</li>
-                    </ul>
-                    <button class="removeBtn" @click="fnRemove(idx)">X</button>
-                </div>
-            </div>
-        </div>
-        <div class="result" v-show="isShow">
-            <h4>{{ result }}</h4>
-            <div class="reset">
-                <button class="resetBtn" @click="fnReset">초기화</button>
-            </div>
-        </div>
-        <div class="confirm">
-            <button class="confirmBtn" @click="fnRandom">{{ isShow ? '다시하기' : '확인' }}</button>
-
-        </div>
+  <div class="container">
+    <!-- header start -->
+    <div class="header">
+      <div class="title">메뉴고르기</div>
+      <div class="sub">🍕🍔🍟🌭🍿🥞🥖🫓🥪🍗🥟</div>
     </div>
+    <!-- header end -->
+
+    <!-- contents start -->
+    <div class="contents">
+      <div class="form">
+        <input
+          v-model="newMenu"
+          class="input"
+          placeholder="메뉴를 입력하세요"
+          @keyup.enter="fnRegister"
+        />
+        <button class="registerBtn" @click="fnRegister">등록</button>
+        <button class="setValueBtn" @click="fnSet">기본값</button>
+      </div>
+      <div class="menu">
+        <div class="menuitem" v-for="(item, idx) in menuGroup" :key="idx">
+          <!-- <ul class="text"> -->
+          <li>{{ item.name }}</li>
+          <button class="removeBtn" @click="fnRemove(idx)">X</button>
+          <!-- </ul> -->
+        </div>
+      </div>
+    </div>
+    <!-- contents end -->
+
+    <!-- bottom start -->
+    <div class="bottom">
+      <div class="result" v-show="isShow">
+        <h4>{{ result }}</h4>
+      </div>
+      <button class="confirmBtn" @click="fnRandom">
+        {{ isShow ? "다시하기" : "확인" }}
+      </button>
+      <button v-show="isShow" class="resetBtn" @click="fnReset">초기화</button>
+    </div>
+    <!-- bottom end -->
+  </div>
 </template>
 
 <script>
-import { MainJs } from '@/resources/js';
+// TODO:: 결과 링크 공유 기능 추가
+import { MainJs } from "@/resources/js";
 export default MainJs;
 </script>
 
-<style scoped >
+<style lang="scss" scoped>
 .container {
-    min-height: 600px;
-    margin-top: 100px;
-    /* display: flex;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  min-height: 750px;
+  padding: 20px 15px;
+  border: 1px solid red;
+  background-color: #fff;
+
+  button {
+    border: none;
+    outline: none;
+    background-color: inherit;
+    cursor: pointer;
+  }
+
+  .header {
     align-items: center;
-    justify-content: center; 
-    flex-direction: column; */
+    justify-content: center;
+    text-align: center;
+    width: 100%;
+    height: 60px;
+    margin-top: 10px;
+    padding: 10px;
+    top: 0;
+    position: fixed;
+    z-index: 1000;
+    background-color: #fff;
 
+    .title {
+      font-size: 25px;
+      font-weight: 800;
+    }
+    .sub {
+      font-size: 20px;
+      margin-top: 10px;
+    }
+  }
+
+  .contents {
+    margin: 60px 0 80px 0;
+    width: 100%;
+    min-height: 600px;
+    border: 1px solid green;
     .form {
-        padding: 20px 0 20px 0;
-        height: 100px;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      padding: 20px 0 20px 0;
+      height: 60px;
+      width: 100%;
 
-        .input {
-            margin-right: 10px; 
-            height: 50px;
-            padding: 0 5px; 
-            font-size: 15px;
-        }
+      .input {
+        width: 200px;
+        margin-right: 10px;
+        height: 40px;
+        padding: 0 5px;
+        font-size: 15px;
+      }
 
-        .registerBtn {
-            margin-right: 10px;
-            height: 50px;
-            width: 40px;
-            font-size: 15px;
-        }
-        .setValueBtn { 
-            height: 50px;
-            width: 50px; 
-            font-size: 15px;
+      .registerBtn {
+        height: 40px;
+        width: 40px;
+        margin-right: 10px;
+        font-size: 16px;
+        font-weight: 800;
+        border-radius: 5px;
+        color: rgb(243, 86, 24);
+      }
+      .registerBtn:active {
+        background-color: rgb(252, 138, 93);
+        color: #fff;
+      }
+
+      .setValueBtn {
+        height: 40px;
+        width: 60px;
+        font-size: 16px;
+        font-weight: 800;
+        color: rgb(118, 118, 117);
+      }
+      .setValueBtn:active {
+        background-color: rgb(118, 118, 117);
+        color: #fff;
+      }
     }
 
-}
+    .reset {
+      margin: 10px 0;
+      .resetBtn {
+        width: 70px;
+        height: 30px;
+      }
+    }
 
-.confirm {
+    .menu {
+      min-height: 200px;
+      display: flex;
+      flex-direction: column;
+
+      li {
+        list-style: none;
+      }
+      .menuitem {
+        display: flex;
+        justify-content: space-between;
+        margin: 5px 0;
+        width: 100%;
+      }
+    }
+  }
+
+  .bottom {
     margin: 10px 0;
+    bottom: 0;
+    width: 100%;
+    position: fixed;
+    z-index: 1000;
+    background: #fff;
+    align-items: center;
+    justify-content: center;
+    height: 70px;
+
+    .result {
+      margin-bottom: 40px;
+      height: 30px;
+      border: 1px solid purple;
+    }
 
     .confirmBtn {
-        width: 70px;
-        height: 30px;
+      height: 40px;
+      width: 100%;
+      font-size: 16px;
+      font-weight: 800;
     }
-}
-.reset {
-    margin: 10px 0;
-    .resetBtn {
-        width: 70px;
-        height: 30px;
-    }
-}
-
-.menu {
-    min-height: 200px;
-    display: flex;
-    flex-direction: column;
-    .menuitem {
-        display: flex;
-        margin: 5px 0;
-        min-width: 100px;
-    }
-    
-
-}
-
-.result {
-    margin-top: 10px;
-}
+  }
 }
 </style>
